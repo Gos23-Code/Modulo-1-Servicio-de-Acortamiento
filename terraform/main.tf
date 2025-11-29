c8dfddc88c758c5eba4010a1747b76d997b85db7
juan david martinez gracia <juandavidmartinez576@gmail.com>
2025-11-29 00:53:02 -0500

    CORS arreglados

--- terraform/main.tf
+++ terraform/main.tf
@@ -149,3 +149,18 @@ output "invoke_url" {
 output "dynamodb_table_name" {
   value = aws_dynamodb_table.shortener_table.name
 }
+
+resource "aws_api_gateway_integration_response" "shorten_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.shortener_api.id
+  resource_id = aws_api_gateway_resource.shorten_resource.id
+  http_method = aws_api_gateway_method.shorten_options.http_method
+  status_code = aws_api_gateway_method_response.shorten_options_200.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [aws_api_gateway_integration.shorten_options_integration]
+}
